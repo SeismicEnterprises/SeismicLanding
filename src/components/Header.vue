567c37450da7d2ed53f717b91109fd792c871462
Roseyyx <sverrevanhoepen12@outlook.com>
2024-08-19 19:11:53 +0200

    Landing Page and Navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+	<header>
+		<div class="flex py-5 px-3">
+			<div class="flex-grow flex items-center z-20">
+				<NavMenu />
+			</div>
+			<div class="flex items-center">
+				<a href="#">
+					<img class="block lg:hidden" src="images/icon-menu.svg" alt="Menu" />
+				</a>
+				<div class="hidden lg:flex lg:space-x-10 lg:items-center">
+					<div class="hover:text-almost-black cursor-pointer">Forums</div>
+					<div class="border-2 hover:border-almost-black hover:text-almost-black rounded-xl px-4 py-2 cursor-pointer">Get Seismic</div>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+import NavMenu from './NavMenu.vue'
+
+export default {
+	name: 'Header',
+	components: {
+		NavMenu
+	},
+	data() {
+		return {}
+	}
+}
+</script>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,17 @@
+<template>
+	<nav>
+		<ul class="hidden lg:flex lg:space-x-7 lg:ml-14">
+			<li class="cursor-pointer hover:text-almost-black">Home</li>
+			<li class="cursor-pointer hover:text-almost-black">About</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'NavMenu',
+	data() {
+		return {}
+	}
+}
+</script>
